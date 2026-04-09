1c58086e2f4c878ff94693e1bff1c108305f5459
Eric Hooks <ericbhooks@outlook.com>
2026-04-08 23:48:45 -0500

    Created the program entry point.

---
+++ PawnSim/SRC/PawnSim.c
@@ -0,0 +1,13 @@
+/**
+ * File name: PawnSim.c
+ * Author: Eric Hooks, 2026
+ * Purpose: Entry point for the PawnSim game.
+ */
+
+ #include <stdio.h>
+
+ int main() {
+	printf("Test");
+
+	return 0;
+ }
